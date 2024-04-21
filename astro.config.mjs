1bb1552e54bb0fb6c7bb8e0492f448aa34b44c02
Kaya Cook <kmcook@usc.edu>
2024-04-21 09:15:56 -0700

    fix routing again

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   build: {
-    // Example: Generate `page.html` instead of `page/index.html` during build.
     format: 'file'
   },
+  site: 'https://rhizostomae.github.io',
+  base: '/kaya-astro-portfolio'
 });
